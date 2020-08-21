e532d9e338e988eda04511ab5cc637c0823bc41b
atranarta <radyuk.tatyana@gmail.com>
2020-08-21 17:28:50 +0200

    Added loader

--- src/components/getCurrentPositionWeather.js
+++ src/components/getCurrentPositionWeather.js
@@ -1,22 +1,37 @@
-const getCurrentPositionWeather = (weather, setWeather, API_key) => {
+const API_key = process.env.REACT_APP_API_KEY;
+
+const getCurrentPositionWeather = (setLoading, setWeather) => {
+
+  let isLoading = false;
+
   const success = position => {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    getWeather(latitude, longitude);
+    if (!isLoading) {
+      getWeather(latitude, longitude);
+    }
   };
 
   navigator.geolocation.getCurrentPosition(success);
 
   const getWeather = (latitude, longitude) => {
     if (latitude !== '' && longitude !== '') {
+      setLoading(true);
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=metric`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (weather === null) {
-            setWeather(data);
+        .then((res) => {
+          setLoading(false);
+          
+          if (!res.ok) {
+            throw Error(res.statusText);
           }
-        });
+    
+          return res.json()
+        })
+        .then((data) => {
+          setWeather(data);
+        })
+        .catch((error) => console.log(error));
     }
   }
 };
--- src/components/weatherFetch.js
+++ src/components/weatherFetch.js
@@ -1,25 +1,67 @@
 import React, { useState } from "react";
 
-import dateBuilder from "./dateBuilder"
-import backgroundChange from "./backgroundChange"
-import getCurrentPositionWeather from "./getCurrentPositionWeather"
-import cityNameSearch from "./cityNameSearch"
+import dateBuilder from "./dateBuilder";
+import backgroundChange from "./backgroundChange";
+import getCurrentPositionWeather from "./getCurrentPositionWeather";
+import cityNameSearch from "./cityNameSearch";
+
+import Loader from "./../images/loader.gif";
 
 const WeatherFetch = () => {
-  const API_key = process.env.REACT_APP_API_KEY;
 
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
-  getCurrentPositionWeather(weather, setWeather, API_key);
+  if (weather === null) {
+    getCurrentPositionWeather(setLoading, setWeather);
+  }
 
   const onKeyPressHandler = evt => {
     if (evt.key === "Enter") {
-      cityNameSearch(setWeather, setQuery, query, API_key);
+      cityNameSearch(setWeather, setQuery, setLoading, query);
     }
   }
 
+  console.log(weather)
   const backgroundStyle = weather !== null ? backgroundChange(weather.weather[0].id) : {};
+
+  const weatherBlock = (data) => {
+
+    if (data === null) {
+      return (
+        <p className="error">Something goes wrong. Retry later.</p>
+      );
+    }
+
+    if (isLoading) {
+      return (
+        <img className="loader" src={Loader} alt="loading" />
+      );
+    }
+
+    return (
+      <>
+        <div className="location-box">
+          <div className="location">{data.name}, {data.sys.country}</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temperature-box">
+            <div className="temperature">{Math.round(data.main.temp)} &#176;C</div>
+            <div className="feelsLike">Feels Like: {Math.round(data.main.feels_like)} &#176;C</div>
+          </div>
+          <div className="weather">
+            <div className="main">{data.weather[0].main}</div>
+            <div className="description">{data.weather[0].description}</div>
+          </div>
+          <div className="icon">
+            <img alt="" src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
+          </div>
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className="app" style={backgroundStyle}>
@@ -33,27 +75,9 @@ const WeatherFetch = () => {
             onKeyPress={onKeyPressHandler}
           />
         </div>
-        {(weather !== null) ? (
-          <>
-            <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temperature-box">
-                <div className="temperature">{Math.round(weather.main.temp)} &#176;C</div>
-                <div className="feelsLike">Feels Like: {Math.round(weather.main.feels_like)} &#176;C</div>
-              </div>
-              <div className="weather">
-                <div className="main">{weather.weather[0].main}</div>
-                <div className="description">{weather.weather[0].description}</div>
-              </div>
-              <div className="icon">
-                <img alt="" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
-              </div>
-            </div>
-          </>
-        ) : ('')}
+        <div className="loader-wrapper">
+          {weatherBlock(weather)}
+        </div>
       </main>
     </div>
   )
