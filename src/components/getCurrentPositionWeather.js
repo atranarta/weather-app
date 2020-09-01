80ffc90af156f09296f300fb2b4bd39dc60a6c9a
Noushad PP <to.noushadpp@gmail.com>
2020-09-01 19:48:26 +0200

    Add async await code

--- src/components/getCurrentPositionWeather.js
+++ src/components/getCurrentPositionWeather.js
@@ -1,24 +1,25 @@
-const getCurrentPositionWeather = (weather, setWeather, API_key) => {
-  const success = position => {
+import { API_key } from '../constants';
+
+const navigatorCall = () => new Promise((resolve) => {
+  navigator.geolocation.getCurrentPosition((position) => {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
+    resolve([latitude, longitude]);
+  });
+});
 
-    getWeather(latitude, longitude);
-  };
-
-  navigator.geolocation.getCurrentPosition(success);
-
-  const getWeather = (latitude, longitude) => {
-    if (latitude !== '' && longitude !== '') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=metric`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (weather === null) {
-            setWeather(data);
-          }
-        });
-    }
+const getWeather = async (latitude, longitude) => {
+  if (latitude !== '' && longitude !== '') {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=metric`);
+    const data = await res.json();
+    return data;
   }
+}
+
+export const getCurrentPositionWeather = async () => {
+  const [latitude, longitude] = await navigatorCall();
+  const weather = await getWeather(latitude, longitude);
+  return weather;
 };
 
 export default getCurrentPositionWeather;
--- src/components/weatherFetch.js
+++ src/components/weatherFetch.js
@@ -1,31 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import dateBuilder from "./dateBuilder"
-import backgroundChange from "./backgroundChange"
-import getCurrentPositionWeather from "./getCurrentPositionWeather"
-import cityNameSearch from "./cityNameSearch"
+import dateBuilder from "./dateBuilder";
+import backgroundChange from "./backgroundChange";
+import getCurrentPositionWeather from "./getCurrentPositionWeather";
+import cityNameSearch from "./cityNameSearch";
 
 const WeatherFetch = () => {
   const API_key = process.env.REACT_APP_API_KEY;
 
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const [weather, setWeather] = useState(null);
 
-  getCurrentPositionWeather(weather, setWeather, API_key);
+  useEffect(() => {
+    const callFunction = async () => {
+      setIsLoading(true);
+      const newWeather = await getCurrentPositionWeather();
+      setWeather(newWeather);
+      setIsLoading(false);
+    };
+
+    callFunction();
+  }, [query]);
 
   const onKeyPressHandler = evt => {
     if (evt.key === "Enter") {
       cityNameSearch(setWeather, setQuery, query, API_key);
     }
-  }
+  };
 
-  const backgroundStyle = weather !== null ? backgroundChange(weather.weather[0].id) : {};
+  const backgroundStyle =
+    weather !== null ? backgroundChange(weather.weather[0].id) : {};
+  
+  if (isLoading) { 
+    return (
+      <div> data loading....</div>
+    );
+  }
 
   return (
     <div className="app" style={backgroundStyle}>
       <main>
         <div className="search-box">
-          <input type="text"
+          <input
+            type="text"
             className="search-bar"
             placeholder="Enter the city..."
             onChange={e => setQuery(e.target.value)}
@@ -33,30 +51,43 @@ const WeatherFetch = () => {
             onKeyPress={onKeyPressHandler}
           />
         </div>
-        {(weather !== null) ? (
+        {weather !== null && weather.weather !== null ? (
           <>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temperature-box">
-                <div className="temperature">{Math.round(weather.main.temp)} &#176;C</div>
-                <div className="feelsLike">Feels Like: {Math.round(weather.main.feels_like)} &#176;C</div>
+                <div className="temperature">
+                  {Math.round(weather.main.temp)} &#176;C
+                </div>
+                <div className="feelsLike">
+                  Feels Like: {Math.round(weather.main.feels_like)} &#176;C
+                </div>
               </div>
               <div className="weather">
                 <div className="main">{weather.weather[0].main}</div>
-                <div className="description">{weather.weather[0].description}</div>
+                <div className="description">
+                  {weather.weather[0].description}
+                </div>
               </div>
               <div className="icon">
-                <img alt="" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                <img
+                  alt=""
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                />
               </div>
             </div>
           </>
-        ) : ('')}
+        ) : (
+          ""
+        )}
       </main>
     </div>
-  )
+  );
 };
 
 export default WeatherFetch;
